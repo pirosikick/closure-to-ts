c142302d15e0319a83a042fbecdf6053392eda36
Hiroyuki Anai <pirosikick@gmail.com>
2019-12-17 23:55:49 +0900

    add importPath

--- __testfixtures__/deps.js
+++ __testfixtures__/deps.js
@@ -1,2 +1,4 @@
 goog.addDependency('../../app/a.js', ['app.a'], ['app.b'], {});
 goog.addDependency('../../app/b.js', ['app.b'], [], {});
+goog.addDependency('../../app/b/c.js', ['app.b.c'], [], {});
+goog.addDependency('../../hoge/d.js', ['hoge.d'], [], {});
